f7a180762a7bca709f4725a783b2c486ca6c03c1
Jo Wagner <jowagner@limago.de>
2021-10-01 14:55:33 +0200

    Letzter Stand

---
+++ MagischesQuadrat/MagischesQuadrat/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagischesQuadrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Bitte Kantenlänge eingeben:");
+            //int kantenlaenge = int.Parse(Console.ReadLine());
+
+            //MagischesQuadrat mq = new MagischesQuadrat(kantenlaenge);
+
+            //mq.Fuellen();
+            //mq.Ausgeben();
+
+            string a = "kurz";
+            string b = "mittelmittel";
+            string c = "langlanglanglanglanglanglanglang";
+
+
+            Console.Write("String a= {0,-33} xxx", a);
+            Console.WriteLine("String b= {0,-33} xxx", b);
+            Console.WriteLine("String c= {0, -33} xxx", c);
+
+        }
+    }
+}
---
+++ Tag5_02MaxWert/Tag5_02MaxWert/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tag5_02MaxWert
+{
+    class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            var x = 0.1;
+            var a = 9.0;
+            var b = a + 1;
+            var c = a + b;
+            double[] feld = {10.5,20,30};
+
+            foreach (var value in feld)
+            {
+                Console.WriteLine(value);
+            }
+            
+        }
+    }
+}
